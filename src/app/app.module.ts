6b5dc29171923ac13fd6f3600b1d7e55ac695007
salonidarji <salonidarji@gmail.com>
2019-04-21 22:52:42 +0530

    template converted to angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderBannerComponent } from './header-banner/header-banner.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBannerComponent,
+    NavComponent,
+    ProductComponent,
+    MainContentComponent,
+    ContactComponent,
+    AboutComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
